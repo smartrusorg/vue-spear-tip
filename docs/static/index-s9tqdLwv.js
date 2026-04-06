3b4d0e6ddd155f4b6e266b1e5cd4cb724f048fe0
CHORNY <den@chorny.ru>
2026-04-06 12:55:43 +0300

    Docs update

--- docs/static/index-s9tqdLwv.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["static/CodeHighlight-DjDPz-Xr.js","static/main-C7NODcnK.js","static/main-DFr6SfZT.css","static/CodeHighlight-CGrGqjhz.css"])))=>i.map(i=>d[i]);
-import{d as e,_ as o}from"./main-C7NODcnK.js";const _=e(()=>o(()=>import("./CodeHighlight-DjDPz-Xr.js"),__vite__mapDeps([0,1,2,3])));export{_ as default};
