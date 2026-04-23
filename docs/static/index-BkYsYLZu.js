6152bfdec817479d71f3fecfd262569a6ff08875
CHORNY <den@chorny.ru>
2026-04-23 12:50:01 +0300

    Docs update

---
+++ docs/static/index-BkYsYLZu.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["static/CodeHighlight-zMX08Owx.js","static/main-BIm_e04l.js","static/main-CeuDnZwd.css","static/CodeHighlight-CGrGqjhz.css"])))=>i.map(i=>d[i]);
+import{d as e,_ as o}from"./main-BIm_e04l.js";const _=e(()=>o(()=>import("./CodeHighlight-zMX08Owx.js"),__vite__mapDeps([0,1,2,3])));export{_ as default};
